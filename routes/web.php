35a10a7635ad8376ffc0be9ea60c52b422ddf94c
Mark van den Broek <mvdnbrk@protonmail.com>
2025-08-13 19:24:04 +0200

    Add route constraints

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,13 @@ use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomepageController::class);
-Route::get('/block/{hashOrHeight}', [BlockController::class, 'show'])->name('block.show');
-Route::get('/tx/{txid}', [TransactionController::class, 'show'])->name('transaction.show');
+
+Route::get('/block/{hashOrHeight}', [BlockController::class, 'show'])
+    ->name('block.show')
+    ->where('hashOrHeight', '[0-9]+|[0-9a-fA-F]{64}');
+
+Route::get('/tx/{txid}', [TransactionController::class, 'show'])
+    ->name('transaction.show')
+    ->where('txid', '[0-9a-fA-F]{64}');
+
 Route::get('/address/{address}', [AddressController::class, 'show'])->name('address.show');
