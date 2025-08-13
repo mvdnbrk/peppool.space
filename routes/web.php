593bdcacd10b9c4c745849a2ffd15ef670095b4e
Mark van den Broek <mvdnbrk@protonmail.com>
2025-08-13 21:08:27 +0200

    Add adreess constraint

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ Route::get('/tx/{txid}', [TransactionController::class, 'show'])
     ->name('transaction.show')
     ->where('txid', '[0-9a-fA-F]{64}');
 
-Route::get('/address/{address}', [AddressController::class, 'show'])->name('address.show');
+Route::get('/address/{address}', [AddressController::class, 'show'])
+    ->name('address.show')
+    ->where('address', 'P[1-9A-HJ-NP-Za-km-z]{25,33}');
