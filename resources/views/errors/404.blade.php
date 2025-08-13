5a50155228717a4cbe7aba960b4b65bc328e4033
Mark van den Broek <mvdnbrk@protonmail.com>
2025-08-13 21:00:33 +0200

    Add custom error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <main class="flex-grow flex items-center justify-center px-4 py-12 sm:py-24">
+        <div class="text-center">
+            <p class="text-base font-semibold text-green-700">404</p>
+            <h1 class="mt-4 text-2xl font-semibold tracking-tight text-balance text-gray-900 sm:text-4xl">
+                Page not found
+            </h1>
+            <p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl">
+                Sorry, we couldn’t find the page you’re looking for.
+            </p>
+        </div>
+    </main>
+</x-layout>
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <main class="flex-grow flex items-center justify-center px-4 py-12 sm:py-24">
+        <div class="text-center">
+            <p class="text-base font-semibold text-green-700">503</p>
+            <h1 class="mt-4 text-2xl font-semibold tracking-tight text-balance text-gray-900 sm:text-4xl">
+                Service Unavailable
+            </h1>
+            <p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl">
+                We're performing some maintenance. Please check back soon.
+            </p>
+        </div>
+    </main>
+</x-layout>
