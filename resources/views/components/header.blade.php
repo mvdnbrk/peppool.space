8ff0d6be70e768b142795ab84b056c8218a322c6
Mark van den Broek <mvdnbrk@protonmail.com>
2025-08-14 13:38:00 +0200

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,11 @@
-<header class="bg-white shadow-sm">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="py-4 sm:py-6">
+<header class="bg-white relative">
+    <div class="w-full h-6 overflow-hidden -mt-px text-green-700">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none" class="w-full h-full">
+            <path fill="currentColor" d="M0,224L48,224C96,224,192,224,288,197.3C384,171,480,117,576,117.3C672,117,768,171,864,202.7C960,235,1056,245,1152,234.7C1248,224,1344,192,1392,176L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+        </svg>
+    </div>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
+        <div class="py-2 sm:py-4">
             <div class="flex justify-between items-center">
                 <!-- Logo and Site Title -->
                 <a href="{{ route('homepage') }}" class="flex items-center">
@@ -21,5 +26,10 @@
                 @endif
             </div>
         </div>
+    </div>
+    <div class="bg-gray-50 w-full h-6 overflow-hidden -mt-px text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none" class="w-full h-full transform scale-x-[-1]">
+            <path fill="currentColor" d="M0,224L48,224C96,224,192,224,288,197.3C384,171,480,117,576,117.3C672,117,768,171,864,202.7C960,235,1056,245,1152,234.7C1248,224,1344,192,1392,176L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+        </svg>
     </div>
 </header>
