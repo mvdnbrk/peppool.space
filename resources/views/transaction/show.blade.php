5ce6aa35e9bf53aebb5900b76ac69a91fb51311a
Mark van den Broek <mvdnbrk@protonmail.com>
2025-08-14 14:32:39 +0200

    change color

--- resources/views/transaction/show.blade.php
+++ resources/views/transaction/show.blade.php
@@ -117,7 +117,7 @@
                 <div class="bg-white rounded-lg shadow p-6">
                     <div class="flex items-center">
                         <div class="flex-shrink-0">
-                            <div class="w-10 h-10 bg-purple-500 rounded-xl flex items-center justify-center">
+                            <div class="w-10 h-10 bg-orange-500 rounded-xl flex items-center justify-center">
                                 <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4"/>
                                 </svg>
