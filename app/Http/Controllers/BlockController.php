9a70e9226e5df93b13e06098222441a267811660
Mark van den Broek <mvdnbrk@protonmail.com>
2025-08-14 15:46:45 +0200

    Refactor

--- app/Http/Controllers/BlockController.php
+++ app/Http/Controllers/BlockController.php
@@ -9,37 +9,61 @@ class BlockController extends Controller
 {
     public function show(PepecoinRpcService $rpc, string $hashOrHeight): View
     {
-        // Convert height to hash if needed
-        $blockHash = is_numeric($hashOrHeight)
-            ? $rpc->getBlockHash((int) $hashOrHeight)
-            : $hashOrHeight;
-
-        // Get block details with error handling
-        try {
-            $block = $rpc->getBlock($blockHash, 2);
-        } catch (\Exception $e) {
-            abort(404, 'Block not found');
-        }
-
-        // Get navigation hashes
-        $prevBlockHash = $block['height'] > 0
-            ? $rpc->getBlockHash($block['height'] - 1)
-            : null;
-
-        // Get next block hash if it exists
-        $nextBlockHash = null;
-        try {
-            $nextBlockHash = $rpc->getBlockHash($block['height'] + 1);
-        } catch (\Exception $e) {
-            // Next block doesn't exist yet
-        }
+        $blockHash = $this->resolveBlockHash($rpc, $hashOrHeight);
+        $block = $this->getBlockOrFail($rpc, $blockHash);
+        $navigation = $this->getNavigationHashes($rpc, $block['height']);
 
         return view('block.show', [
             'block' => $block,
             'blockHeight' => $block['height'],
             'blockHash' => $blockHash,
-            'prevBlockHash' => $prevBlockHash,
-            'nextBlockHash' => $nextBlockHash,
+            'prevBlockHash' => $navigation['prev'],
+            'nextBlockHash' => $navigation['next'],
         ]);
+    }
+
+    private function resolveBlockHash(PepecoinRpcService $rpc, string $hashOrHeight): string
+    {
+        try {
+            if (is_numeric($hashOrHeight)) {
+                $height = (int) $hashOrHeight;
+                if ($height < 0) {
+                    abort(404, 'Block not found');
+                }
+                return $rpc->getBlockHash($height);
+            }
+            return $hashOrHeight;
+        } catch (\Exception $e) {
+            abort(404, 'Block not found');
+        }
+    }
+
+    private function getBlockOrFail(PepecoinRpcService $rpc, string $blockHash): array
+    {
+        try {
+            return $rpc->getBlock($blockHash, 2);
+        } catch (\Exception $e) {
+            abort(404, 'Block not found');
+        }
+    }
+
+    private function getNavigationHashes(PepecoinRpcService $rpc, int $height): array
+    {
+        $prevBlockHash = $height > 0 ? $this->getBlockHashSafely($rpc, $height - 1) : null;
+        $nextBlockHash = $this->getBlockHashSafely($rpc, $height + 1);
+
+        return [
+            'prev' => $prevBlockHash,
+            'next' => $nextBlockHash,
+        ];
+    }
+
+    private function getBlockHashSafely(PepecoinRpcService $rpc, int $height): ?string
+    {
+        try {
+            return $rpc->getBlockHash($height);
+        } catch (\Exception $e) {
+            return null;
+        }
     }
 }
