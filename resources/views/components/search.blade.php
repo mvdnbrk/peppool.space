3f0d7aec344abc75ca43be04831a11a1fdeaa957
Mark van den Broek <mvdnbrk@protonmail.com>
2025-08-15 13:44:34 +0200

    change cursor

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -13,7 +13,7 @@
                 autocomplete="off"
                 required
             />
-            <button type="submit" class="px-4 bg-green-700 text-white text-sm font-medium hover:bg-green-800 focus:outline-none inline-flex items-center justify-center">
+            <button type="submit" class="px-4 bg-green-700 text-white text-sm font-medium hover:bg-green-800 focus:outline-none inline-flex items-center justify-center cursor-pointer">
                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35m1.1-4.4a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
