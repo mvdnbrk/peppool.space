a3fb8f411b56ef9ef8a4d072988037bac7cb666b
Mark van den Broek <mvdnbrk@protonmail.com>
2025-08-24 18:37:04 +0200

    code style

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('throttle:60,1')->name('api.')->group(function () {
     Route::get('/blocks/tip/height', [BlockController::class, 'tipHeight'])->name('blocks.tip.height');
     Route::get('/blocks/tip/hash', [BlockController::class, 'tipHash'])->name('blocks.tip.hash');
+    Route::get('/prices', PricesController::class)->name('prices');
+
     Route::prefix('mempool')->name('mempool.')->group(function () {
         Route::get('/', [MempoolController::class, 'index'])->name('index');
         Route::get('/txids', [MempoolController::class, 'txids'])->name('txids');
     });
-    Route::get('/prices', PricesController::class)->name('prices');
 });
