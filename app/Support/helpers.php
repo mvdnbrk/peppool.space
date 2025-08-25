38684a26028c651ca417281afed1c9fb970223e6
Mark van den Broek <mvdnbrk@protonmail.com>
2025-08-25 13:24:34 +0200

    add helpers

---
+++ app/Support/helpers.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Number;
+
+if (!function_exists('format_hashrate')) {
+    function format_hashrate(float $hashrate): string {
+        $units = ['H/s', 'KH/s', 'MH/s', 'GH/s', 'TH/s', 'PH/s', 'EH/s'];
+        $i = 0;
+
+        while ($hashrate >= 1000 && $i < count($units) - 1) {
+            $hashrate /= 1000;
+            $i++;
+        }
+
+        return Number::format($hashrate, 2) . ' ' . $units[$i];
+    }
+}
+
+if (!function_exists('format_difficulty')) {
+    function format_difficulty(float $difficulty): string {
+        $units = ['', 'K', 'M', 'G', 'T', 'P', 'E'];
+        $i = 0;
+
+        while ($difficulty >= 1000 && $i < count($units) - 1) {
+            $difficulty /= 1000;
+            $i++;
+        }
+
+        return Number::format($difficulty, 2) . ' ' . $units[$i];
+    }
+}
