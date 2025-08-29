7f6260905eb844deee9102b8938ec848d3d7a3c2
Mark van den Broek <mvdnbrk@protonmail.com>
2025-08-29 15:30:37 +0200

    adjust color scheme

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,10 +2,10 @@
     <main class="flex-grow flex items-center justify-center px-4 py-12 sm:py-24">
         <div class="text-center">
             <p class="text-base font-semibold text-green-700">503</p>
-            <h1 class="mt-4 text-2xl font-semibold tracking-tight text-balance text-gray-900 sm:text-4xl">
+            <h1 class="mt-4 text-2xl font-semibold tracking-tight text-balance text-gray-900 dark:text-white sm:text-4xl">
                 Service Unavailable
             </h1>
-            <p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl">
+            <p class="mt-6 text-lg font-medium text-pretty text-gray-500 dark:text-gray-400 sm:text-xl">
                 We are performing some maintenance. Please check back soon.
             </p>
         </div>
