77b36f39991dc7d2a0a81c602eeabd38ee8e77dd
Mark van den Broek <mvdnbrk@protonmail.com>
2025-08-29 12:38:11 +0200

    code style

--- app/View/Components/Svg/PizzaNinjasPepe.php
+++ app/View/Components/Svg/PizzaNinjasPepe.php
@@ -6,17 +6,11 @@ use Illuminate\View\Component;
 
 class PizzaNinjasPepe extends Component
 {
-    /**
-     * Create a new component instance.
-     */
     public function __construct()
     {
         //
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render()
     {
         return view('components.svg.pizza-ninjas-pepe');
