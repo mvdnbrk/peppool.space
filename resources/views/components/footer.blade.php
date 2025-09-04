dd6c470bd9d5dd25762402be4a5498db877cbb66
Mark van den Broek <mvdnbrk@protonmail.com>
2025-09-04 18:06:42 +0200

    add search link

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -25,6 +25,9 @@
                 <li>
                     <a href="{{ route('docs.api') }}" title="Pepecoin API Documentation" aria-label="Pepecoin API documentation" class="inline-flex items-center rounded-full bg-white text-green-800 hover:text-green-900 px-3 py-1.5 md:px-3 md:py-1.5 lg:px-2.5 lg:py-1 font-semibold shadow-sm ring-1 ring-inset ring-white/60 hover:ring-emerald-300 hover:bg-emerald-50/70 transition-colors">API</a>
                 </li>
+                <li>
+                    <a href="{{ route('search.index') }}" title="Search" aria-label="Search" class="inline-flex items-center rounded-full bg-white text-green-800 hover:text-green-900 px-3 py-1.5 md:px-3 md:py-1.5 lg:px-2.5 lg:py-1 font-semibold shadow-sm ring-1 ring-inset ring-white/60 hover:ring-emerald-300 hover:bg-emerald-50/70 transition-colors">Search</a>
+                </li>
             </ul>
         </nav>
 
