1a80cc9df0fadcd567aaf353c28a08d8615c344d
Mark van den Broek <mvdnbrk@protonmail.com>
2025-09-04 18:04:52 +0200

    Update index.blade.php

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -1,6 +1,8 @@
 <x-layout title="Search - peppool.space" og_description="Search the Pepecoin blockchain: find blocks, transactions, and addresses quickly on peppool.space.">
     <div class="max-w-2xl mx-auto my-16">
-        <h1 class="text-2xl font-bold text-gray-900 dark:text-gray-300 mb-6">Search</h1>
+        <h1 class="text-xl md:text-2xl font-bold text-gray-900 dark:text-gray-200 mb-6">
+            Search the Pepecoin blockchain
+        </h1>
         <x-search />
     </div>
 </x-layout>
