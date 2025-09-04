e292f7e65b0c1f9435b8e86af6718d77fcc5eefc
Mark van den Broek <mvdnbrk@protonmail.com>
2025-09-04 18:24:06 +0200

    Create ViewApiDocsPageTest.php

---
+++ tests/Feature/ViewApiDocsPageTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ViewApiDocsPageTest extends TestCase
+{
+    public function test_api_docs_page_can_be_viewed(): void
+    {
+        $this->get('/docs/api')
+            ->assertStatus(200)
+            ->assertSee('Pepecoin API Documentation')
+            ->assertSee('Base URL')
+            ->assertSee('Rate Limiting');
+    }
+
+    public function test_api_docs_page_lists_key_endpoints(): void
+    {
+        $this->get('/docs/api')
+            ->assertStatus(200)
+            ->assertSee('/blocks/tip/hash')
+            ->assertSee('/blocks/tip/height')
+            ->assertSee('/mempool')
+            ->assertSee('/mempool/txids')
+            ->assertSee('/prices')
+            ->assertSee('/validate-address/:address');
+    }
+}
