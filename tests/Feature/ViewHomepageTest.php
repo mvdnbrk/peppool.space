16578db40882eb7a6bda2e5f2a65717cd06c89a5
Mark van den Broek <mvdnbrk@protonmail.com>
2025-09-05 14:18:16 +0200

    Create ViewHomepageTest.php

---
+++ tests/Feature/ViewHomepageTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class ViewHomepageTest extends TestCase
+{
+    // TODO: implement test
+
+    // #[Test]
+    // public function a_request_to_the_homepage_should_return_a_200_status_code(): void
+    // {
+    //     $this->get(route('homepage'))
+    //         ->assertOk()
+    //         ->assertViewIs('homepage');
+    // }
+}
