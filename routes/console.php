459fe7f45a70c83ec5bd736f475ac4ead812c0bc
Mark van den Broek <mvdnbrk@protonmail.com>
2025-12-11 15:50:52 +0100

    disable syncing transactions

--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,5 @@ Schedule::job(FetchPepePrice::class)->everyFifteenMinutes();
 Schedule::job(StorePepePrice::class)->everyFifteenMinutes();
 
 Schedule::command('pepe:sync:blocks')->everyMinute();
-Schedule::command('pepe:sync:transactions')->hourly();
+// Schedule::command('pepe:sync:transactions')->hourly();
 Schedule::job(CalculateTotalSupply::class)->hourly();
