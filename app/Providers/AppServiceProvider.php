508b3a2a8e1eb4c4b751ca37347e768f753f1bfd
Mark van den Broek <mvdnbrk@protonmail.com>
2026-02-26 11:57:43 +0100

    Add BlockchainService bindings to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
+use App\Contracts\BlockchainServiceInterface;
+use App\Services\BlockchainService;
 use App\Services\ElectrsPepeService;
 use App\Services\PepecoinExplorerService;
 use App\Services\PepecoinPriceService;
 use App\Services\PepecoinRpcService;
+use App\Services\RpcBlockchainService;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,9 +17,13 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->singleton(PepecoinRpcService::class);
+        $this->app->singleton(ElectrsPepeService::class);
+        $this->app->singleton(RpcBlockchainService::class);
+        $this->app->singleton(BlockchainService::class);
         $this->app->singleton(PepecoinExplorerService::class);
         $this->app->singleton(PepecoinPriceService::class);
-        $this->app->singleton(ElectrsPepeService::class);
+
+        $this->app->bind(BlockchainServiceInterface::class, BlockchainService::class);
     }
 
     public function boot(): void
